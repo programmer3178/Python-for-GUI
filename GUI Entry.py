4d1e2dfe693e51b15cc7aec5b40a92d7a790d8e9
sys <shritejashastrys.works@gmail.com>
2024-12-19 21:16:36 +0530

    first commit

--- GUI Entry.py
+++ GUI Entry.py
@@ -1,34 +1,35 @@
 from tkinter import *
 
+
 def submit():
     username = entry.get()
     print("Hello" + " " + username)
 
+
 def delete():
     entry.delete(0, END)
 
+
 def backspace():
-   entry.delete(len(entry.get()) - 1, END)
+    entry.delete(len(entry.get()) - 1, END)
 
 
 window = Tk()
 
-submit = Button(window, text = 'Submit', command =submit)
-submit.pack(side = RIGHT)
+submit = Button(window, text='Submit', command=submit)
+submit.pack(side=RIGHT)
 
-delete = Button(window, text = 'Delete', command = delete)
-delete.pack(side = RIGHT)
+delete = Button(window, text='Delete', command=delete)
+delete.pack(side=RIGHT)
 
-backspace = Button(window, text = 'Backspace', command = backspace)
-backspace.pack(side = RIGHT)
-
-
+backspace = Button(window, text='Backspace', command=backspace)
+backspace.pack(side=RIGHT)
 
 entry = Entry()
-entry.config(font = ('Ink Free', 50), bg = '#000000', fg = '#00FF00')
-#entry.insert(10, "Hey!")
-#entry.config(state = DISABLED)
-entry.config(width = 30)
-#entry.config(show = '.')
+entry.config(font=('Ink Free', 50), bg='#000000', fg='#00FF00')
+# entry.insert(10, "Hey!")
+# entry.config(state = DISABLED)
+entry.config(width=30)
+# entry.config(show = '.')
 entry.pack()
 window.mainloop()
--- GUI Label.py
+++ GUI Label.py
@@ -1,19 +1,19 @@
 from tkinter import *
 
 window = Tk()
-window.config(background= "#FFD700")
-photo = PhotoImage(file = "logo.png")
+window.config(background="#FFD700")
+photo = PhotoImage(file="logo.png")
 label = Label(window,
               text="Hello World",
               font=('TimesNewRoman', 40, 'bold'),
               fg="green",
               background="black",
-              relief= RAISED,
-              bd= 10,
-              padx = 20,
-              pady = 20,
-              image = photo,
-              compound= "bottom")
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound="bottom")
 
 label.pack()
 
