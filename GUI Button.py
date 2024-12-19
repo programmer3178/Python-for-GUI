f7691241e4c7d532b476ad3d3e18f138d3b7c379
sys <shritejashastrys.works@gmail.com>
2024-12-19 12:33:23 +0530

    first commit

---
+++ GUI Button.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+count = 0
+
+
+def click():
+    global count
+    count = count + 1
+    count_label.config(text=count)
+
+
+window = Tk()
+
+button = Button(window, text='click me!')
+button.config(command=click, font=('Ink Free', 50, 'bold'), bg="orange", fg="black")
+button.config(activebackground='#FF0000', activeforeground='#FFA500')
+image = PhotoImage(file='logo.png')
+button.config(image=image, compound='top', state=ACTIVE)
+count_label = Label(window, text=count, font=('Monospace', 50, 'bold'))
+count_label.config(bg='white')
+count_label.pack()
+button.pack()
+window.mainloop()
